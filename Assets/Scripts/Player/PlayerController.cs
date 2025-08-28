354d73fda6075e5f05a3cc3d0035007f058d6233
khb7 <onebeankalfka@gmail.com>
2025-08-28 17:21:18 +0900

    fix: findcollectiblegems

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -122,7 +122,7 @@ public class PlayerController : MonoBehaviour
                 CollectClosestMineable(); // Renamed from CollectClosestGem
             }
 
-            FindCollectibleMineables(); // Renamed from FindCollectibleGems
+            FindCollectibleGems(); // Renamed from FindCollectibleGems
         }
     }
 
@@ -186,7 +186,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void FindCollectibleMineables() // Renamed from FindCollectibleGems
+    private void FindCollectibleGems() // Renamed from FindCollectibleGems
     {
         collectibleMineables.Clear();
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, collectionRadius);
