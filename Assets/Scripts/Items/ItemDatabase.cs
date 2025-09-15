3f7adbc9713756e48d3bbc6d5e26132de5316a83
yaho1231 <hiimjungle@naver.com>
2025-09-15 20:38:44 +0900

    feat : minealDB
    1. Minerals, Item related changes and additions
    2. Fix main menu bugs

---
+++ Assets/Scripts/Items/ItemDatabase.cs
@@ -0,0 +1,41 @@
+
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "ItemDatabase", menuName = "Database/Item Database")]
+public class ItemDatabase : ScriptableObject
+{
+    public static ItemDatabase Instance { get; private set; }
+
+    public List<ItemSO> allItems;
+
+    private Dictionary<MineralID, ItemSO> itemDictionary;
+
+    private void OnEnable()
+    {
+        Instance = this;
+
+        itemDictionary = new Dictionary<MineralID, ItemSO>();
+        if (allItems != null)
+        {
+            foreach (var item in allItems)
+            {
+                if (item != null && item.MineralID != MineralID.None && !itemDictionary.ContainsKey(item.MineralID))
+                {
+                    itemDictionary.Add(item.MineralID, item);
+                }
+            }
+        }
+    }
+
+    public ItemSO GetItemByID(MineralID id)
+    {
+        if (itemDictionary == null)
+        {
+            OnEnable();
+        }
+
+        itemDictionary.TryGetValue(id, out ItemSO item);
+        return item;
+    }
+}
--- Assets/Scripts/Items/Mineable.cs
+++ Assets/Scripts/Items/Mineable.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class Mineable : MonoBehaviour
 {
-    public Item itemData; // PlayerController가 접근해야 하는 데이터
+    public ItemSO itemData; // PlayerController가 접근해야 하는 데이터
 }
